1d847a39c263d2dd420aa10c81a99d60bc673701
Luke Harris <l_harris4@fanshaweonline.ca>
2018-04-30 17:20:48 -0400

    cleaning physics library, added filters for clarity

--- GDP2017-18-master/GDP_Feeney_201718/globalOpenGL_GLFW.h
+++ GDP2017-18-master/GDP_Feeney_201718/globalOpenGL_GLFW.h
@@ -1,18 +1,6 @@
 #ifndef _globalOpenGL_GLFW_HG_
 #define _globalOpenGL_GLFW_HG_
 
-// glad, glm, and GLFW need to be included in a certain order,
-//	or the build is going to 'crash and burn'
-
-// Including this file in anything that 'touches' anything related
-//	to OpenGL will ensure that things are included in the correct order. 
-
-// NOTE: glm does NOT require glad, GLFW, or gl.h, so you can include it 
-//	anywhere you need it.
-
-// SHORT VERSION OF THIS: If you need ANYTHING with OpenGL (constant, functions, whatever), 
-//                         then include THIS file, not the glad and GLFW files specifically)
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
